8e760e3688fbdd01a811ff7f8e9357ac4ccdb49a
Frank Francione <frankiefrancione@gmail.com>
2024-08-01 16:33:43 -0500

    made some ui progress

--- src/components/InvestigatorStats.jsx
+++ src/components/InvestigatorStats.jsx
@@ -4,65 +4,131 @@ const test = "test";
 
 const InvestigatorStats = () => {
   return (
-    <div id="characteristics" className=" p-6 bg-slate-300">
-      <h2 className="text-lg font-bold">Characteristics:</h2>
-      <form action="" className="m-4 ">
-        <div className="grid grid-cols-3 ">
-          <div>
-            <label htmlFor="">STR: </label>
-            <input type="number" min={15} max={90} />
+    <div id="characteristics" className=" bg-inherit">
+      <div className="m-4 px-4">
+        <h2 className="text-3xl font-black ">Characteristics</h2>
+        <form action="" className="">
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <div>
+              <label className="font-bold" htmlFor="">
+                STR:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                DEX:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                INT:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+
+            <div>
+              <label className="font-bold" htmlFor="">
+                CON:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                APP:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                POW:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+
+            <div>
+              <label className="font-bold" htmlFor="">
+                SIZ:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                EDU:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <div>
+              <label className="font-bold" htmlFor="">
+                LUCK:{" "}
+              </label>
+              <input
+                type="number"
+                min={15}
+                max={90}
+                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="">DEX: </label>
-            <input type="number" min={15} max={90} />
+          <div className="my-5 flex justify-center">
+            <button className="w-1/3 bg-indigo-600 py-1 min-w-20 text-lg text-slate-100 rounded-md hover:bg-indigo-800 transition-all duration-100">
+              Roll Again
+            </button>
           </div>
-          <div>
-            <label htmlFor="">INT: </label>
-            <input type="number" min={15} max={90} />
-          </div>
+        </form>
+        <div id="derived-characteristics">
+          <h3 className="text-xl font-bold">Derived Stats</h3>
+          <ul className="grid grid-cols-3">
+            <li className="font-bold">SAN: {test}</li>
+            <li className="font-bold">HP: {test}</li>
+            <li className="font-bold">MP: {test}</li>
+            <li className="font-bold">MOVE: {test}</li>
+            <li className="font-bold">BUILD: {test}</li>
+            <li className="font-bold">Damage Bonus: {test}</li>
+            <li className="font-bold">Personal Interest Points: {test}</li>
+          </ul>
         </div>
-        <div className="grid grid-cols-3">
-          <div>
-            <label htmlFor="">CON: </label>
-            <input type="number" min={15} max={90} />
-          </div>
-          <div>
-            <label htmlFor="">APP: </label>
-            <input type="number" min={15} max={90} />
-          </div>
-          <div>
-            <label htmlFor="">POW: </label>
-            <input type="number" min={15} max={90} />
-          </div>
-        </div>
-        <div className="grid grid-cols-3">
-          <div>
-            <label htmlFor="">SIZ: </label>
-            <input type="number" min={15} max={90} />
-          </div>
-          <div>
-            <label htmlFor="">EDU: </label>
-            <input type="number" min={15} max={90} />
-          </div>
-          <div>
-            <label htmlFor="">LUCK: </label>
-            <input type="number" min={15} max={90} />
-          </div>
-        </div>
-        <button className=" bg-indigo-600 py-1 min-w-20 text-lg text-slate-100 rounded-md hover:bg-indigo-800 transition-all duration-100">
-          Roll
-        </button>
-      </form>
-      <div id="derived-characteristics">
-        <ul>
-          <li>SAN: {test}</li>
-          <li>HP: {test}</li>
-          <li>MP: {test}</li>
-          <li>MOVE: {test}</li>
-          <li>BUILD: {test}</li>
-          <li>Damage Bonus: {test}</li>
-          <li>Personal Interest Points: {test}</li>
-        </ul>
       </div>
     </div>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="px-8 py-6 ">
+        <h1 className="text-5xl font-black">About Us</h1>
+      </div>
+      <div className=" px-6 py-6 bg-slate-800 flex flex-row">
+        <p className="text-gray-200 p-2">
+          We love H.P. Lovecraft, his stories, his mythos and of course the
+          TTRPG Call of Cthulhu (by Chaosium). This site was built to help Game
+          Masters run their games in a much more streamlined manner. Characters
+          are prone to... death, in Call of Cthulhu and this site is here to
+          help players and keepers generate their own characters in the shortest
+          amount of time. Currently, we offer two types of character generation:
+          Quick Start and Classic. Quick Start reflects the rules given in
+          Chaosium's free Call of Cthulhu starter rules. Classic is the standard
+          way to generate a character.
+        </p>
+        <img
+          src="/images/AncientNecromancer.jpeg"
+          alt="Ancient Necromancer"
+          className="max-h-[500px] max-w-[600px] p-2"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
